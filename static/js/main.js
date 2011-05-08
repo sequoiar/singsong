2bca56b90e9c379a755a8264ea5674f6ffa204ee
James Halliday <substack@gmail.com>
2011-05-08 04:15:15 -0700

    oh whoops wasn't tracking main.js

--- static/js/main.js
+++ static/js/main.js
@@ -118,30 +118,6 @@ $(window).ready(function () {
         revertDuration : 0,
     });
     
-    /*
-    $('#treble').droppable({
-        accept : '.note',
-        drop : function (ev, ui) {
-            var ypx = ev.clientY + (112 - ev.offsetY);
-            var y = Math.floor((ypx - 63) / 33 * 2);
-            var note = {
-                8 : 'E3',
-                7 : 'F3',
-                6 : 'G3',
-                5 : 'A4',
-                4 : 'B4',
-                3 : 'C4',
-                2 : 'D4',
-                1 : 'E4',
-                0 : 'F4',
-            }[y];
-            if (note) {
-                console.log(note);
-            }
-        },
-    });
-    */
-    
     dnode.connect(function (remote) {
         remote.sing([
             {
