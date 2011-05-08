a2aca168d28c051275500141b36109544c34b7a4
James Halliday <substack@gmail.com>
2011-05-08 02:28:23 -0700

    computes notes for the treble clef seemingly correctly

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,32 @@ var dnode = require('dnode');
 var $ = require('jquery-browserify');
 
 $(window).ready(function () {
+    
+    $('.note').draggable({
+        revert : true,
+        revertDuration : 0,
+    });
+    
+    $('#treble').droppable({
+        accept : '.note',
+        drop : function (ev, ui) {
+            var ypx = ev.clientY + (112 - ev.offsetY);
+            var y = Math.floor((ypx - 63) / 33 * 2);
+            var note = {
+                8 : 'E3',
+                7 : 'F3',
+                6 : 'G3',
+                5 : 'A4',
+                4 : 'B4',
+                3 : 'C4',
+                2 : 'D4',
+                1 : 'E4',
+                0 : 'F4',
+            }[y];
+            console.log(note);
+        },
+    });
+    
     dnode.connect(function (remote) {
         remote.sing([
             {
